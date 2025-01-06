cb637ad4630970fd04cefe2540b7e5fbfd96945a
Marco Túlio <marcotulio_iub@hotmail.com>
2025-01-06 16:57:17 -0300

    feat: list users controller

---
+++ src/application/controllers/ListUsersController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { ListUsersUseCase } from "../../domain/usecases/ListUsersUseCase";
+
+export class ListUsersController {
+  constructor(private listUserUseCase: ListUsersUseCase) {
+    
+  }
+
+  handle(req: Request, res: Response) {
+    try {
+      const users = this.listUserUseCase.execute();
+      return res.status(200).json(users);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  }
+}
