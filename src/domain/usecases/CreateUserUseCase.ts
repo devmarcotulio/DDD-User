7923a83bcfbc59c37fda458598140ff9d8d76e20
Marco Túlio <marcotulio_iub@hotmail.com>
2025-01-06 16:56:19 -0300

    feat: create user usecase

---
+++ src/domain/usecases/CreateUserUseCase.ts
@@ -0,0 +1,21 @@
+import { User } from "../entities/User";
+import { UserRepository } from "../repository/UserRepository";
+
+export class CreateUserUseCase {
+  constructor(private userRepository: UserRepository) {
+    
+  }
+
+  execute(name: string, email: string, cpf: string) {
+    const emailExists = this.userRepository.findByEmail(email);
+    if (emailExists) {
+      throw new Error('Email already exists.');
+    }
+    const cpfExists = this.userRepository.findByCpf(cpf);
+    if (cpfExists) {
+      throw new Error('CPF already exists.');
+    }
+    const user = User.create(name, email, cpf);
+    this.userRepository.save(user);
+  }
+}
