bfa3c93ff9e7382f0d0d20786ba5be38e29fdf1f
Marco Túlio <marcotulio_iub@hotmail.com>
2025-01-06 16:56:03 -0300

    feat: create user controller

---
+++ src/application/controllers/CreateUserController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { CreateUserUseCase } from "../../domain/usecases/CreateUserUseCase";
+
+export class CreateUserController {
+  constructor(private createUserUseCase: CreateUserUseCase) {
+    
+  }
+
+  handle(req: Request, res: Response) {
+    try {
+      const { name, email, cpf } = req.body;
+      this.createUserUseCase.execute(name, email, cpf);
+      return res.status(201).end();
+    } catch(err) {
+      if (err instanceof Error) return res.status(400).json({ message: err.message }); 
+      return res.status(500).json({ message: 'Internal server error'});
+    }
+  }
+}
